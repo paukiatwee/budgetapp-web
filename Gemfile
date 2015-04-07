9b1dfcf44c925f1c3a2de6f67b9ef755e88a7d0a
paukiatwee <paukiatwee@gmail.com>
2015-04-07 23:18:23 +0800

    update to use asset pipeline

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'tilt', '1.4.1'
 gem 'rack', '1.5.2'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 
 
 group :development do
--- server.rb
+++ server.rb
@@ -1,6 +1,27 @@
 
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
 
-get '/' do
-  erb :index, :locals => { :title => 'Budget App' }
+class App < Sinatra::Base
+
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',      from: 'app/js'
+    serve '/css',     from: 'app/css'
+    serve '/img',     from: 'app/img'
+
+    js :main, ['/js/script.js']
+    css :main, ['/css/*.css']
+  }
+
+  get '/' do
+    erb :index, :locals => { :title => 'Budget App' }
+  end
+end
+
+if __FILE__ == $0
+  App.run!
 end
